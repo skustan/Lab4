dab03fa761d0cc8be723b121515e3669e145f789
JHoffmanPHD <101751709+JHoffmanPHD@users.noreply.github.com>
2025-03-27 14:13:11 +0100

    Rotate commit - naprawa bledow

--- Form1.cs
+++ Form1.cs
@@ -23,40 +23,38 @@ namespace Lab4
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
                 openFileDialog.Filter = "Image|*.png;*.png;*.bmp;*.gif";
-                if (openFileDialog.ShowDialog() == DialogResult.OK) { }
-                pictureBox1.Image = new Bitmap(Image.FromFile(@openFileDialog.FileName));
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = new Bitmap(Image.FromFile(@openFileDialog.FileName));
+                }
             }
         }
 
         private void buttonRotate_Click(object sender, EventArgs e)
         {
-            if (radioButton90.Checked)
+            try
             {
-                try
+                if (radioButton90.Checked)
                 {
                     pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
                     pictureBox1.Refresh();
+
                 }
-                catch {  }
-            }
-            if (radioButton180.Checked)
-            {
-                try
+                if (radioButton180.Checked)
                 {
+
                     pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
                     pictureBox1.Refresh();
+
                 }
-                catch { }
-            }
-            if (radioButton270.Checked)
-            {
-                try
+                if (radioButton270.Checked)
                 {
+
                     pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
                     pictureBox1.Refresh();
                 }
-                catch { }
             }
+            catch { }
         }
     }
 }
